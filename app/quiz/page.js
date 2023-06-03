019311ef844ab3dcc98813ce33c2fae62efb412e
manlikexavy <chibokaxavier@gmail.com>
2023-06-03 06:22:12 -0700

    ABLE TO ANSWER QUESTIONS UP UNTIL THE LAST ONE, NEXT UP IS TO OUTPUT THE RESULTS

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -15,9 +15,42 @@ const page = () => {
   });
   const { questions } = quiz;
   const { question, answers, correctAnswer } = questions[activeQuestion];
+
   const onAnswerSelected = (answer, index) => {
     setChecked(true);
     setSelectedAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+      console.log("true");
+    } else {
+      setSelectedAnswer(false);
+      console.log("false");
+    }
+  };
+
+  const nextQuestion = () => {
+    setSelectedAnswerIndex(null);
+    setResult((prev) => 
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            incorrectAnswers: prev.incorrectAnswers + 1,
+          }
+    );
+
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion(activeQuestion + 1);
+    } else {
+      setActiveQuestion(0);
+      setShowResult(true);
+      
+    }
+    setChecked(false);
   };
 
   return (
@@ -32,25 +65,42 @@ const page = () => {
       <div>
         {!showResult ? (
           <div className="bg-[#f8f8f8] p-4 mt-8 rounded-[4px] text-black ">
-            <h3 className="text-xl font-bold">
-              {questions[activeQuestion].question}
-            </h3>
+            <h3 className="text-xl font-bold">{question}</h3>
             {answers.map((answer, index) => (
               <li
                 key={index}
                 onClick={() => onAnswerSelected(answer, index)}
                 className={`${
                   selectedAnswerIndex === index
-                    ? "bg-[#000925] text-white"
+                    ? "bg-[#000925] text-white list-none cursor-pointer my-2 py-4 px-2"
                     : "hover:bg-[#d8d8d8] list-none cursor-pointer my-2  text-[#000105] border border-[lightgray] py-4 px-2 "
                 }`}
               >
                 <span>{answer}</span>
               </li>
             ))}
+            {checked ? (
+              <div>
+                <button
+                  onClick={nextQuestion}
+                  className="py-4 px-2 w-full mt-3 rounded-[4px] cursor-pointer bg-[#808080] text-[#f8f8f8]"
+                >
+                  {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button
+                  disabled
+                  className="py-4 px-2 w-full mt-3 cursor-not-allowed rounded-[4px] bg-gray-200 text-[#f8f8f8]"
+                ></button>
+              </div>
+            )}
           </div>
         ) : (
-          <div className="bg-[#f8f8f8] p-4 mt-8 rounded-[4px] text-black"></div>
+          <div
+            className="bg-[#f8f8f8] p-4 mt-8 rounded-[4px] text-black"
+          ></div>
         )}
       </div>
     </div>
